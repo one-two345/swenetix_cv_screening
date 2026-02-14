c7f99c74db502439ede7d9c1a9201153246563ba
one-two345 <teklemariamshewamnil@gmail.com>
2026-02-14 10:59:54 +0300

    code base

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './db.config.js'; 
+import { createTest } from './model/Test.model.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    connectDB(); // Connect to MongoDB when the server starts
+    createTest({ name: 'Sample Test', score: 95 }) // Example of creating a test entry
+        .then(test => console.log('Test created:', test))
+        .catch(err => console.error('Error creating test:', err));
+    console.log(`Server is running on port ${PORT}`);
+}); 
