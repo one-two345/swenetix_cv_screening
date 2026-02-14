c62af57b443d27065d1efe7791aa079d2bd55548
Zahir-Seid <zahirseid101@gmil.com>
2026-02-14 14:27:33 +0300

    remove test model

--- server/model/Test.model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const TestSchema = new mongoose.Schema({
-  name: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model('Test', TestSchema);
